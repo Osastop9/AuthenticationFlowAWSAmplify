a7320a820d17e4fa9c91b1e57bc59f94f717c21e
Isaac Uduehi <osayi4jesus@gmail.com>
2022-09-25 16:22:27 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
